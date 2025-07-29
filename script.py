fd03a423ddd77c1ced277134b8e2792fc0d07890
nek00oo <yakovenkovalera2017@yandex.ru>
2025-07-29 17:53:44 +0300

    jn

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 def get_user_input():
     print("1")
     name = input("Enter your username: ")
+    print(name)
     length = float(input("Enter rectangle length: "))
     width = float(input("Enter rectangle width: "))
-    print("Hi")
+    print("Hello")
     return name, length, width
